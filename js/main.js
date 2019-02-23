624fa5e75e3d7c590f6660118e71541fb1d25ced
Dinosaur DBA <dinosaurdba@gmail.com>
2019-02-23 16:28:10 +0000

    Previous commit almost worked. Delay didn't change - seemed to stick with default. Removed default.

--- js/main.js
+++ js/main.js
@@ -55,6 +55,6 @@ button.addEventListener('click', function(e) {
             if (--i) {
                 theLoop(i);
             }
-        }, document.getElementById("speakDelay").value*1000 || 1500);
+        }, document.getElementById("speakDelay").value*1000;
     })(shuffledkbs.length);
 });
